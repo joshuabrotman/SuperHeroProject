aacfa3e8754aa3b11b36c079ce89d798e82a14fd
Joshuabrotman <joshuabrotman@Gmail.com>
2020-04-21 13:08:15 -0500

    changed the bootstrap from default

--- SuperHero/Startup.cs
+++ SuperHero/Startup.cs
@@ -62,7 +62,7 @@ namespace SuperHero
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Superhero}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
